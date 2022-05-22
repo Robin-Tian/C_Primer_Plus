15299a60d654b6020be1c79769af4155bdfb101f
RobinTian0916 <RobinTian0916@gmail.com>
2022-05-22 22:12:41 +0800

    change_dic

--- Chapter_11/tempCodeRunnerFile.c
+++ Chapter_11/tempCodeRunnerFile.c
@@ -1,46 +1,20 @@
- /* join_chk.c -- joins two strings, check size first */
+ /* copy2.c -- strcpy() demo */
  #include <stdio.h>
- #include <string.h>
- #define SIZE 30
- #define BUGSIZE 13
- char * s_gets(char * st, int n);
+ #include <string.h> // declares strcpy()
+ #define WORDS "beast"
+ #define SIZE 40
+ 
  int main(void)
  {
-    char flower[SIZE];
-    char addon[] = "s smell like old shoes.";
-    char bug[BUGSIZE];
-    int available;
-    
-    puts("What is your favorite flower?");
-    s_gets(flower, SIZE);
-    if ((strlen(addon) + strlen(flower) + 1) <= SIZE)
-        strcat(flower, addon);
-    puts(flower);
-    puts("What is your favorite bug?");
-    s_gets(bug, BUGSIZE);
-    available = BUGSIZE - strlen(bug) - 1;
-    strncat(bug, addon, available);
-    puts(bug);
+    const char * orig = WORDS;
+    char copy[SIZE] = "Be the best that you can be.";
+    char * ps;
+ 
+    puts(orig);
+    puts(copy);
+    ps = strcpy(copy + 7, orig);
+    puts(copy);
+    puts(ps);
  
     return 0;
- }
- 
- char * s_gets(char * st, int n)
- {
-    char * ret_val;
-    int i = 0;
- 
-    ret_val = fgets(st, n, stdin);
-    if (ret_val)
-    {
-        while (st[i] != '\n' && st[i] != '\0')
-            i++;
-        if (st[i] == '\n')
-            st[i] = '\0';
-        else // must have words[i] == '\0'
-            while (getchar() != '\n')
-                continue;
-    }
- 
-    return ret_val;
  }
